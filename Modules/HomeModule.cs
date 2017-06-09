8672c123df60b60b1276c31d74f0436951813254
kimlan nguyen <kimlan1510@yahoo.com>
2017-06-09 13:17:16 -0700

    Create Nancy route to add_client.cshtml, add_stylist.cshtml. Write Post[] to get information form the forms

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,6 +17,27 @@ namespace HairSalon
         List<Clients> AllClients = Clients.GetAll();
         return View["clients.cshtml", AllClients];
       };
+      Get["/clients/new"] = _ =>  {
+        List<Stylists> AllStylists = Stylists.GetAll();
+        return View["/add_client.cshtml", AllStylists];
+      };
+      Get["/stylists/new"] = _ =>  {
+        return View["/add_stylist.cshtml"];
+      };
+      Post["/clients"]= _ =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Clients newClients = new Clients(Request.Form["client"], Request.Form["stylist_id"]);
+        var selectedStylist = Stylists.Find(newClients.GetStylistId());
+        newClients.Save();
+        model.Add("stylist", selectedStylist);
+        model.Add("client", newClients);
+        return View["client_added.cshtml", model];
+      };
+      Post["/stylists"]= _ =>{
+        Stylists newStylists = new Stylists(Request.Form["name"]);
+        newStylists.Save();
+        return View["stylist_added.cshtml", newStylists];
+      };
 
     }
   }
